3594a0e90569460b71e77bccf754ffdd6f95cd52
WilliamSCastro <103841892+WilliamSCastro@users.noreply.github.com>
2024-09-24 13:47:04 +0200

    Update ex2.php

--- ex2.php
+++ ex2.php
@@ -12,14 +12,14 @@
         <tr>
             <?php
                 $n = 30;
-                for($i=65;$i<=$n+65;$i++) {
+                for($i=65;$i<$n+65;$i++) {
                     echo "<td style='border: 1px solid black; border-collapse: collapse; padding: 15px;'>".chr($i)."</td>";
                 }
             ?>
         </tr>
         <tr>
             <?php
-                for($i=0;$i<=$n;$i++) {
+                for($i=0;$i<$n;$i++) {
                     echo "<td style='border: 1px solid black; border-collapse: collapse; padding: 15px;'>$i</td>";
                 }
             ?>
